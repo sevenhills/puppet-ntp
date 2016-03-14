dadc5ae12711395f26d57d3bac31003df83f444a
vkorpolu <vkorpolu@cisco.com>
2016-03-13 18:58:16 -0700

    update spec for centos

--- modules/ntp/spec/classes/ntp_spec.rb
+++ modules/ntp/spec/classes/ntp_spec.rb
@@ -3,19 +3,14 @@ require 'spec_helper'
 describe 'ntp' do
 
   let(:title) { 'ntp' }
-  let(:node) { 'precise32' }
+  let(:node) { 'centos65' }
   let :facts do
     {
-      :ipaddress                 => '10.42.42.42',
       :id                        => 'root',
       :osfamily                  => 'Debian',
       :kernel                    => 'Linux',
       :operatingsystem           => 'Debian',
-      #:operatingsystemrelease    => '7.8',
-      #:operatingsystemmajrelease => '7',
-      #:virtual                   => 'physical',
       :path                      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      #:concat_basedir            => '/dne',
     }
   end
 
@@ -26,7 +21,7 @@ describe 'ntp' do
     it { should contain_service('ntp').with_enable('true') }
     it { should contain_file('ntp.conf').with_ensure('file') }
     it 'should generate a valid default template' do
-      should contain_file('ntp.conf').with_content(/server 1.pool.ntp.org/)
+      should contain_file('ntp.conf').with_content(/server 0.rhel.pool.ntp.org/)
     end
   end
 end
